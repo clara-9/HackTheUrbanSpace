570314d71fa93c83928bbb57877e54f89d362359
clara <clara.rull@estudiantat.upc.edu>
2021-01-24 00:09:51 +0100

    work up to now

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import requests
 from io import BytesIO
 from google.cloud import storage
 import uuid 
+from datetime import datetime
+
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import dict_factory
+
+cloud_config= {
+        'secure_connect_bundle': 'secure-connect-hackcambridge.zip'
+}
+
+auth_provider = PlainTextAuthProvider('clara', 'helloclara')
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect('dataspace')
+
 
 import os
 
@@ -43,17 +57,37 @@ def upload_to_bucket(img_name, path_to_file):
 @app.route("/send_image", methods=['POST'])
 def bird_capture():
     file=request.files['file']
+    lat=request.args.get("lat")
+    lon=request.args.get("lon")
     print(file)
     path=os.path.join("/tmp/", file.name)
     file.save(path)
-    img_id=id_generator()+".jpg"
+    img_id=id_generator()
     image_url=upload_to_bucket(img_id, path)
+    now = datetime.now()
+    print(now)
+    session.execute("""INSERT INTO dataspace.hostile_arch (image_id, latitude, longitude, timestamp, user_id) 
+                    VALUES (%s, %s, %s,%s, 'admin')", (img_id, float(lat), float(lon), now))"""
     return img_id
 
 @app.route("/img_id_creator")
 def id_generator():
-    img_id=str(uuid.uuid1())
+    img_id=str(uuid.uuid4())
     return img_id
+
+@app.route("/hostile_data")
+def go_cassandra():
+    session.row_factory = dict_factory
+    rows = session.execute("select * from hostile_arch").all()
+    #take into account url for image downlad is https://storage.googleapis.com/hostile-images and the id
+    print(rows)
+    if rows:
+        print(rows)
+    else:
+        print("An error occurred.")
+    return {"rows":rows}
+
+        
 
 if __name__ == "__main__":
     app.run(host = '127.0.0.1', port = 8080, debug=True)
