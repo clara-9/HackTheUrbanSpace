7522a2c4bf218f01c2bff73fbf7166997a97bc3b
Pitr, Michal <pitrmich@fel.cvut.cz>
2021-01-24 08:52:08 +0000

    add predict route

---
+++ src/predict.py
@@ -0,0 +1,13 @@
+from fastai.learner import load_learner
+
+path = './models/resnet152.pkl'
+
+
+class Model:
+    def __init__(self, path):
+        self.learn = load_learner(path)
+        classes = self.learn.dls.vocab
+
+    def predict_single_image(self, img_file):
+        prediction = self.learn.predict(img_file)
+        return prediction
